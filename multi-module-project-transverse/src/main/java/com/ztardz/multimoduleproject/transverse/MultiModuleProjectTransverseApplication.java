fe228d83d47d3f1e1b0add10c0e1a3071ce229e1
TarekZERGAT <t.zergat@gmail.com>
2022-10-26 22:42:32 +0100

    Creation du Module Transverse

---
+++ multi-module-project-transverse/src/main/java/com/ztardz/multimoduleproject/transverse/MultiModuleProjectTransverseApplication.java
@@ -0,0 +1,13 @@
+package com.ztardz.multimoduleproject.transverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModuleProjectTransverseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiModuleProjectTransverseApplication.class, args);
+    }
+
+}
---
+++ multi-module-project-transverse/src/test/java/com/ztardz/multimoduleproject/transverse/MultiModuleProjectTransverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ztardz.multimoduleproject.transverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiModuleProjectTransverseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
