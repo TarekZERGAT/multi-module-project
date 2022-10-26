4d49c7cb15b64eba960534bc05abc35575290404
TarekZERGAT <t.zergat@gmail.com>
2022-10-26 22:40:15 +0100

    Creation du Module Controller

---
+++ multi-module-project-controller/src/test/java/com/ztardz/multimoduleprojectcontroller/MultiModuleProjectControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ztardz.multimoduleprojectcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiModuleProjectControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
