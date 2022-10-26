8e35f3fb4966c446910db4cdd00d4906b9990a13
TarekZERGAT <t.zergat@gmail.com>
2022-10-26 22:35:08 +0100

    Creation du Module Persistance

---
+++ multi-module-project-persistance/src/main/java/com/ztardz/multimoduleproject/persistance/MultiModuleProjectPersistanceApplication.java
@@ -0,0 +1,13 @@
+package com.ztardz.multimoduleproject.persistance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModuleProjectPersistanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiModuleProjectPersistanceApplication.class, args);
+    }
+
+}
---
+++ multi-module-project-persistance/src/test/java/com/ztardz/multimoduleproject/persistance/MultiModuleProjectPersistanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ztardz.multimoduleproject.persistance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiModuleProjectPersistanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
