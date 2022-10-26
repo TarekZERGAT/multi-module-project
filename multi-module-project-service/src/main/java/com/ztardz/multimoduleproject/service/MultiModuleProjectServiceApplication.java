d168aecdf240b74c348103c2609fd1bf69f7af3e
TarekZERGAT <t.zergat@gmail.com>
2022-10-26 22:37:13 +0100

    Creation du Module Service

---
+++ multi-module-project-service/src/main/java/com/ztardz/multimoduleproject/service/MultiModuleProjectServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ztardz.multimoduleproject.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModuleProjectServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiModuleProjectServiceApplication.class, args);
+    }
+
+}
